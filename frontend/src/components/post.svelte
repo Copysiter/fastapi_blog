0703e51a99e86e67234343ba2f15795db45b50d6
p-bzh <ronan.questier@mailfence.com>
2022-05-27 00:02:57 +0300

    add post details

---
+++ frontend/src/components/post.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { Post } from "../stores/posts";
+	export let post: Post;
+</script>
+
+<a href={`/posts/${post.slug}`}><h2>{post.title}</h2></a>
+<p>{post.body}</p>
---
+++ frontend/src/routes/posts/[slug].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  // @ts-ignore
+  export const load = async ({params}) => {
+    console.log(typeof params)
+    const slug = params.slug;
+    const url = `http://127.0.0.1:8000/posts/${slug}`;
+    const response = await fetch(url);
+    const post = await response.json();
+    return {props : {post}};
+  }
+</script>
+<script lang="ts">
+  import type { Post} from "../../stores/posts"
+  export let  post : Post;
+</script>
+
+<h2>{post.title}</h2>
