b6c21b0b1a34a49eec8d0b8ddb327655d0301719
p-bzh <ronan.questier@mailfence.com>
2022-05-27 00:03:59 +0300

    add component for post

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,17 +1,14 @@
 <svelte:head>
-  <title>My Blog</title>
+	<title>My Blog</title>
 </svelte:head>
 
 <h1>Blog</h1>
 
- 
-  {#each $posts as post}
-<h2>{post.title}</h2>
-<p>{post.body}</p>
+{#each $posts as post}
+	<PostComponent {post} />
 {/each}
 
-
 <script lang="ts">
-  import  {posts} from "../stores/posts";
+	import { posts } from "../stores/posts";
+	import PostComponent from "../components/post.svelte";
 </script>
-
