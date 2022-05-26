e64849a68b0b45d7319c9fb8dc18341ec4c2d10f
p-bzh <ronan.questier@mailfence.com>
2022-05-26 19:53:24 +0300

    add cors

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio
 import os
 from dotenv import load_dotenv
@@ -8,6 +9,17 @@ load_dotenv()
 
 app = FastAPI()
 app.include_router(router=posts_router, prefix="/posts")
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.on_event("startup")
