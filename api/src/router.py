e57b2e90e02606e8a171ce8eb65c9eb894f613a8
p-bzh <ronan.questier@mailfence.com>
2022-05-26 19:52:49 +0300

    fix database

--- api/src/router.py
+++ api/src/router.py
@@ -10,20 +10,20 @@ router = APIRouter()
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def publish_post(request: Request, post: PostModel = Body(...)):
     post = jsonable_encoder(post)
-    new_post = await request.database["posts"].insert_one(post)
-    created_post = await request.database["posts"].find_one({"_id": new_post.inserted_id})
+    new_post = await request.app.database["posts"].insert_one(post)
+    created_post = await request.app.database["posts"].find_one({"_id": new_post.inserted_id})
     return created_post
 
 
 @router.get("/", response_model=List[PostModel])
 async def get_posts(request: Request):
-    posts = await request.database["posts"].find().to_list(50)
+    posts = await request.app.database["posts"].find().to_list(50)
     return posts
 
 
 @router.get("/{slug}", response_model=PostModel)
 async def get_post(request: Request, slug: str):
-    post = await request.database["posts"].find_one({"slug": slug})
+    post = await request.app.database["posts"].find_one({"slug": slug})
     if post is not None:
         return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -35,12 +35,12 @@ async def update_post(request: Request, id: str, post: UpdatedPostModel = Body(.
     post = {key: value for key, value in post.dict().items()
             if value is not None}
     if len(post) >= 1:
-        update_result = await request.database["posts"].update_one({"_id": id}, {"$set": post})
+        update_result = await request.app.database["posts"].update_one({"_id": id}, {"$set": post})
         if update_result.modified_count == 1:
-            updated_post = await request.database["posts"].find_one({"_id": id})
+            updated_post = await request.app.database["posts"].find_one({"_id": id})
             if updated_post is not None:
                 return updated_post
-    existing_post = await request.database["posts"].find_one({"_id": id})
+    existing_post = await request.app.database["posts"].find_one({"_id": id})
     if existing_post is not None:
         return existing_post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -49,7 +49,7 @@ async def update_post(request: Request, id: str, post: UpdatedPostModel = Body(.
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(request: Request, id: str):
-    delete_result = await request.database["posts"].delete_one({"_id": id})
+    delete_result = await request.app.database["posts"].delete_one({"_id": id})
     if delete_result.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
