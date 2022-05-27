5db4f65ce15b440f13654ca3d9907241e7c6e07e
p-bzh <ronan.questier@mailfence.com>
2022-05-27 06:46:07 +0300

    edit post component

--- frontend/src/components/post.svelte
+++ frontend/src/components/post.svelte
@@ -1,10 +1,10 @@
-<script lang="ts">
-	import type { Post } from "../stores/posts";
-	export let post: Post;
-</script>
-
 <a href={`/posts/${post.slug}`}><h2>{post.title}</h2></a>
 {#each post.categories as category}
 <span>{category}</span>
 {/each}
 <p>{post.body}</p>
+
+<script lang="ts">
+	import type { Post } from "../stores/posts";
+	export let post: Post;
+</script>
